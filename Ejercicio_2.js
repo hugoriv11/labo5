d3d803a30739805dd5a10a10a2c797340009e9ef
hugoriv11 <00161417@uca.edu.sv>
2018-09-21 09:02:47 -0600

    firstt

---
+++ Ejercicio_2.js
@@ -0,0 +1,9 @@
+function ordenar (array) {
+    for (i=0 ; i<=array.length; i++){
+        if (i<i+1){
+            console.log(i);
+        } else {
+            console.log("aasdad")
+        }
+    }
+}
